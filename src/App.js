27d13435ac9a442440240edd91548c5db7a76a9d
Ignacio Aguirre <ignacio13542@gmail.com>
2021-06-23 12:40:11 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+//api key: 60ca59d386a747acbcd68bfa7a8fec8e
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello this will be a spotify clone app!!!!!!!</h1>
+      {/*/spotify logo*/}
+      {/*login page*/}
     </div>
   );
 }
